0e1bd7d277dbe7b924695157ad1c26bdb3d5b06d
Kamil GÖRÜCÜ <98644346+KamilGorucu@users.noreply.github.com>
2025-09-08 19:43:25 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,7 @@
-import path from 'path';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  sassOptions: {
-    includePaths: [path.join(process.cwd(), 'styles')],
-    quietDeps: true,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'storage.googleapis.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'firebasestorage.googleapis.com',
-      },
-    ],
-  },
+  reactStrictMode: true,
+  swcMinify: true,
 };
 
 export default nextConfig;
