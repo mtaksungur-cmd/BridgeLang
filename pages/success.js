669142de89265911aac5f19cf775323218fe6ea6
Kamil GÖRÜCÜ <98644346+KamilGorucu@users.noreply.github.com>
2025-09-08 13:01:30 +0300

    Update success.js

--- pages/success.js
+++ pages/success.js
@@ -1,9 +1,10 @@
+import Link from "next/link";
 export default function Success() {
   return (
     <div style={{ padding: 40 }}>
       <h1>Payment Successful ✅</h1>
       <p>Your payment has been processed successfully.</p>
-      <a href="/student/dashboard">Go to Dashboard</a>
+      <Link href="/student/dashboard">Go to Dashboard</Link>
     </div>
   );
 }
