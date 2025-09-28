4f8de56a5e4c70be17b763d076ac2c6cc337fab2
mtaksungur-cmd <mtaksungur@gmail.com>
2025-09-28 16:37:53 +0300

    Update apply-teaching specializations.js

--- pages/teacher/apply.js
+++ pages/teacher/apply.js
@@ -32,7 +32,7 @@ export default function TeacherApply() {
     languagesSpoken: '',
     experienceYears: '',
     educationLevel: '',
-    lessonTypes: '',
+    teachingSpecializations: '',
     studentAges: '',
     availability: '',
     pricing30: '',
