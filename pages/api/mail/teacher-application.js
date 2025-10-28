7a195eb92e4465a4bf3b2c56d634303b690e4282
mtaksungur-cmd <mtaksungur@gmail.com>
2025-10-28 20:45:05 +0300

    Update teacher-application.js
    BridgeLang Team

--- pages/api/mail/teacher-application.js
+++ pages/api/mail/teacher-application.js
@@ -15,7 +15,7 @@ export default async function handler(req, res) {
         <p>Thank you for applying to teach on <strong>BridgeLang.</strong>.</p>
         <p>Your application has been received and will be reviewed by our team shortly – usually within a couple of business days.</strong>.</p>
         <p>We’ll email you once your profile is approved or if additional information is needed.</p>
-        <p>Best regards,<br/>BridgeLang Teacher Support Team</p>
+        <p>Best regards,<br/>BridgeLang Team</p>
       `,
     });
 
