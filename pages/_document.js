65abfd7fb898e719446e4bdcabb296e32f8f604a
Kamil GÖRÜCÜ <98644346+KamilGorucu@users.noreply.github.com>
2025-11-27 11:31:21 +0300

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        {/* Google Ads Tag */}
         <script
           async
           src="https://www.googletagmanager.com/gtag/js?id=AW-17608551252"
@@ -19,7 +20,35 @@ export default function Document() {
             `,
           }}
         />
+
+        {/* Meta Pixel Base Code */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window,document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1480078056405993');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=1480078056405993&ev=PageView&noscript=1"
+          />
+        </noscript>
       </Head>
+
       <body>
         <Main />
         <NextScript />
